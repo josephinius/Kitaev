35365da55d8bf4bafdd24015f30e967591325815
josephinius <jozef.genzor@gmail.com>
2020-05-18 17:00:18 +0800

    Implementing Hamiltonian for the Potts model

--- classical.py
+++ classical.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 import ctmrg
 
-# Square-lattice classical Ising model
+# Ising model
 
 
 def hamiltonian_ising_weight(*s, h=.0):  # s1, s2, s3, s4 = s
@@ -25,6 +25,54 @@ def hamiltonian_ising_corner(*s, h=.0, g=.0):  # s1, s2, s3, s4 = s
     return - (z1 * z2 + z2 * z3 + z3 * z4 + z4 * z1) - h * (2 * z1 + z2 + z3 + 2 * z4) / 2 - g * (z1 + z4)
 
 
+# Potts model
+
+
+def delta_function(i, j):
+    return int(i == j)
+
+
+def hamiltonian_potts_weight(*s, h=.0, field_direction=0):
+    s1, s2, s3, s4 = s
+    interaction_terms = - (delta_function(s1, s2) +
+                           delta_function(s2, s3) +
+                           delta_function(s3, s4) +
+                           delta_function(s4, s1))
+    field_terms = - h * (delta_function(s1, field_direction) +
+                         delta_function(s2, field_direction) +
+                         delta_function(s3, field_direction) +
+                         delta_function(s4, field_direction)) / 2
+    return interaction_terms + field_terms
+
+
+def hamiltonian_potts_tm(*s, h=.0, g=.0, h_direction=0, g_direction=0):
+    s1, s2, s3, s4 = s
+    interaction_terms = - (delta_function(s1, s2) +
+                           delta_function(s2, s3) +
+                           delta_function(s3, s4) +
+                           delta_function(s4, s1))
+    global_field_terms = - h * (2 * delta_function(s1, h_direction) +
+                                delta_function(s2, h_direction) +
+                                delta_function(s3, h_direction) +
+                                delta_function(s4, h_direction)) / 2
+    boundary_field_terms = - g * delta_function(s1, g_direction)
+    return interaction_terms + global_field_terms + boundary_field_terms
+
+
+def hamiltonian_potts_corner(*s, h=.0, g=.0, h_direction=0, g_direction=0):
+    s1, s2, s3, s4 = s
+    interaction_terms = - (delta_function(s1, s2) +
+                           delta_function(s2, s3) +
+                           delta_function(s3, s4) +
+                           delta_function(s4, s1))
+    global_field_terms = - h * (2 * delta_function(s1, h_direction) +
+                                delta_function(s2, h_direction) +
+                                delta_function(s3, h_direction) +
+                                2 * delta_function(s4, h_direction)) / 2
+    boundary_field_terms = - g * (delta_function(s1, g_direction) + delta_function(s4, g_direction))
+    return interaction_terms + global_field_terms + boundary_field_terms
+
+
 def initialization(q, temperature, h, g):
 
     w = np.zeros((q, q, q, q))
@@ -40,6 +88,10 @@ def initialization(q, temperature, h, g):
                     corner[j, k] += np.exp(-hamiltonian_ising_corner(i, j, k, l, h=h, g=g) / temperature)
 
     return w, tm, corner
+
+
+# TODO: Clock model
+# TODO: Model of social influence
 
 
 # Initialization...
